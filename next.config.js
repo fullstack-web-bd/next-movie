f7728dc6eec3bba32113eb239bbacc19e417dfe9
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-08-13 20:26:29 +0600

    Added images url supports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+        // pathname: "",
+      },
+      {
+        protocol: "https",
+        hostname: "images-na.ssl-images-amazon",
+        port: "",
+        // pathname: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
