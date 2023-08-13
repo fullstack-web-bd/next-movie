e99b0492f1d808d0b30d25898b8700c75c26d542
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-08-14 00:01:53 +0600

    Movie detail

--- components/movies/movie-details.tsx
+++ components/movies/movie-details.tsx
@@ -1,36 +1,83 @@
+import Link from "next/link";
 import SectionHeader from "../section-header";
+import MovieGalleryImage from "./movie-gallery";
+import { useRouter } from "next/router";
+import { Badge } from "flowbite-react";
 
-export default function () {
-    return (
-        <>
-            <SectionHeader title='Plot' areaClassName='flex-col'>
-                <div className="tex-xs">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias in porro, harum sequi dolorem eos et veritatis dolore unde saepe, necessitatibus animi quo voluptatibus suscipit culpa maxime reiciendis! Obcaecati, doloremque.
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias in porro, harum sequi dolorem eos et veritatis dolore unde saepe, necessitatibus animi quo voluptatibus suscipit culpa maxime reiciendis! Obcaecati, doloremque.
-                    </p>
-                </div>
-            </SectionHeader>
+export default function ({ movie }) {
+  const router = useRouter();
+  const genres = movie.genre?.split(",") ?? [];
 
-            <SectionHeader title='Director' areaClassName='flex-col'>
-                <div className="tex-xs">
-                    Kelly Marcel
-                </div>
-            </SectionHeader>
+  const goToGenrePage = (genre: string) => {
+    router.push({
+      pathname: "/",
+      query: {
+        genre,
+      },
+    });
+  };
 
-            <SectionHeader title='Production' areaClassName='flex-col'>
-                <div className="tex-xs">
-                    Columbia Pictures
-                </div>
-            </SectionHeader>
+  return (
+    <>
+      <SectionHeader
+        title={`${movie.type === "movie" ? "Movie" : "Series"} type`}
+        areaClassName="flex-col"
+      >
+        <div className="tex-xs">
+          <div className="flex">
+            {genres.map((genre) => (
+              <Link
+                href={`#`}
+                className="text-red hover:text-white mr-2"
+                onClick={() => goToGenrePage(genre)}
+                key={genre}
+              >
+                <Badge>{genre ?? ""}</Badge>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </SectionHeader>
 
-            <SectionHeader title='Cast' areaClassName='flex-col'>
-                <div className="flex flex-wrap gap-2 mt-2">
-                    <img src="https://www.pngkit.com/png/detail/615-6156135_vin-diesel-vin-diesel-fast-and-furious-png.png" className="rounded-full w-20 h-20" />
-                </div>
-            </SectionHeader>
-        </>
-    )
+      <SectionHeader title="Plot" areaClassName="flex-col">
+        <div className="tex-xs">{movie.plot ?? "N/A"}</div>
+      </SectionHeader>
+
+      <SectionHeader title="Director" areaClassName="flex-col">
+        <div className="tex-xs">{movie.director ?? "N/A"}</div>
+      </SectionHeader>
+
+      <SectionHeader title="Writer" areaClassName="flex-col">
+        <div className="tex-xs">{movie.writer ?? "N/A"}</div>
+      </SectionHeader>
+
+      <SectionHeader title="Awards" areaClassName="flex-col">
+        <div className="tex-xs">{movie.awards ?? "N/A"}</div>
+      </SectionHeader>
+
+      <SectionHeader title="Total IMDB votes" areaClassName="flex-col">
+        <div className="tex-xs">{movie.awards ?? "N/A"}</div>
+      </SectionHeader>
+
+      <SectionHeader title="Cast" areaClassName="flex-col">
+        <div className="flex flex-wrap gap-2 mt-2">
+          {/* <img
+            src="https://www.pngkit.com/png/detail/615-6156135_vin-diesel-vin-diesel-fast-and-furious-png.png"
+            className="rounded-full w-20 h-20"
+          /> */}
+          {movie.actors}
+        </div>
+      </SectionHeader>
+
+      <SectionHeader title="Gallery" areaClassName="flex-col">
+        <div className="tex-xs">
+          <div className="flex flex-wrap gap-2 mt-2">
+            {movie.images.map((image) => (
+              <MovieGalleryImage key={image} image={image} />
+            ))}
+          </div>
+        </div>
+      </SectionHeader>
+    </>
+  );
 }
