3aaff531ea5876bfa258e3f998577164e0df1997
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-08-14 00:02:07 +0600

    Coming soon page

---
+++ pages/coming-soon.tsx
@@ -0,0 +1,40 @@
+import Movies from "@/components/movies";
+import MovieBanner from "@/components/movie-banner";
+
+export default function ComingSoon({ movies, movieBanner }) {
+  return (
+    <main>
+      <MovieBanner movie={movieBanner} />
+      <Movies movies={movies} title={"Coming Soon"} />
+    </main>
+  );
+}
+
+export async function getServerSideProps({ query, res }) {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=60, stale-while-revalidate=59"
+  );
+
+  const sort = query.sort ?? "sort_rating_desc";
+  let url = `http://localhost:3000/api/movies?sort=${sort}&coming_soon=true`;
+
+  if (query.search) {
+    url += `&search=${query.search}`;
+  }
+
+  const listResponse = await fetch(url);
+  const movies = await listResponse.json();
+
+  let movieBanner = null;
+  if (movies.data.length > 0) {
+    movieBanner = movies.data[Math.floor(Math.random() * movies.data.length)];
+  }
+
+  return {
+    props: {
+      movies: movies.data,
+      movieBanner,
+    },
+  };
+}
